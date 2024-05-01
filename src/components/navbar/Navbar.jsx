0e6daa06af96b1f29f18e0eab2dac9107a7ee369
franklin-raph <igboekwulusifranklin@gmail.com>
2024-05-01 10:12:01 +0100

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  useEffect(() => {
+    const menuOpen = document.querySelector('.ri-menu-3-line')
+    const menuClose = document.querySelector('.ri-close-fill')
+    const menu = document.querySelector('nav ul')
+
+    menuOpen.addEventListener('click', () =>{
+      menu.style.top = '180px'
+      menuOpen.style.display = "none"
+      menuClose.style.display = "block"
+    })
+
+    menuClose.addEventListener('click', () =>{
+      menu.style.top = '-180px'
+      menuClose.style.display = "none"
+      menuOpen.style.display = "block"
+    })
+  },[])
+
   return (
     <nav className='flex items-center justify-between shadow-sm fixed w-full bg-white z-10 px-[5rem] py-4'>
       <Link>
         {/* <img src="./vite.svg" alt="" /> */}
         Logo.
       </Link>
+        <div className="togglers hidden text-[20px]">
+          <i class="ri-menu-3-line"></i>
+          <i class="ri-close-fill hidden"></i>
+        </div>
       <ul className='flex items-center justify-between gap-7'>
         <li>
           <Link to='/'>Home</Link>
